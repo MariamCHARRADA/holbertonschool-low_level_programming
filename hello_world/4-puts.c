ca8b3d38a32b6f1a45f3517fd4b380a3f227bbbd
MariamCHARRADA <charradamariam@yahoo.com>
2023-06-16 13:40:59 -0700

    puts

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
 /**
- *int main always void
- *return alwys takes space bfr (
- * */
+ * int main always void
+ * return alwys takes space bfr (
+ */
 int main(void)
-	return (0);
 {
-puts("Programming is like building a multilingual puzzle\n");
+	puts("Programming is like building a multilingual puzzle\n");
+	return (0);
 }
