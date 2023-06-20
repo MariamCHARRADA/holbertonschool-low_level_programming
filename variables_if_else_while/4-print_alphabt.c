448a17a57a2e1f9db5fb7db92581f2d40b02482e
MariamCHARRADA <charradamariam@yahoo.com>
2023-06-20 01:41:33 -0700

    not e not q

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Return: 0
+ */
+int main(void)
+{
+	int ch;
+
+	ch = 'a';
+
+	for (; ch <= 'z' && ch != 'q' && ch != 'e'; ch++)
+		putchar(ch);
+	putchar('\n');
+return (0);
+}
