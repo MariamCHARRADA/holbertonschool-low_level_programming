05f3bc0df3d16195d34793dc0657e84a94ec527a
MariamCHARRADA <charradamariam@yahoo.com>
2023-06-20 01:07:54 -0700

    putchar

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+* main - entry point
+* Return: always 0
+*/
+int main(void)
+{
+	int ch;
+
+	ch = 'a';
+
+	for (; ch <= 'z'; ch++)
+	putchar(ch);
+	putchar('\n');
+
+return (0);
+}
