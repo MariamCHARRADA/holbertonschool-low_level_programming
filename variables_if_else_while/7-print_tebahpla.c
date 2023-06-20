a42b5cd5f1a57dace5fdb16b6959aa5939636d86
MariamCHARRADA <charradamariam@yahoo.com>
2023-06-20 03:14:23 -0700

    cond: ch <= a

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+* main - entry point
+* Return: always 0
+*/
+int main(void)
+{
+	int ch = 'z';
+
+	for (; ch >= 'a'; ch--)
+	putchar(ch);
+	putchar('\n');
+
+return (0);
+}
