d23ec573f709fc85d459e4eb0bf8c77a34fc1d47
MariamCHARRADA <charradamariam@yahoo.com>
2023-06-21 05:58:09 -0700

    jjj

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_alphabet - entry point
+ */
+void print_alphabet_x10(void)
+{
+	char ch = 'a';
+	int n = 0;
+
+	for (; n < 10; n++)
+	{
+	for (; ch <= 'z'; ch++)
+	{	_putchar(ch);
+	}
+	_putchar('\n');
+	}
+}
