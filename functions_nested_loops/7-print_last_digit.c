7b06b101ee08ee6dbf19d80a62a4b0c7babb53c6
MariamCHARRADA <charradamariam@yahoo.com>
2023-06-22 07:22:33 -0700

    last degit

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_last_digit - computes absolute value
+ * @n: parameter
+ * Return: n
+ */
+int print_last_digit(int n)
+{
+	int l;
+
+	if (n < 0)
+	{
+		l = (n * -1) % 10;
+	}
+	else
+	{
+		l = n % 10;
+	}
+return (n);
+}
