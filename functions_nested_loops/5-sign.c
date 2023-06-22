869cffe7c97b902c548182471624c04af65ca980
MariamCHARRADA <charradamariam@yahoo.com>
2023-06-22 04:47:20 -0700

    sign1

---
+++ functions_nested_loops/5-sign.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_sign - print sign
+ * Return: 0 or 1 or -1
+ * @n: parameter
+ */
+int print_sign(int n)
+{
+if (n > 0)
+{	_putchar('+');
+	return (1);
+}
+else if (n == 0)
+{	_putchar('0');
+	return (0);
+}
+else
+{	_putchar('-');
+	return (-1);
+}
+}
