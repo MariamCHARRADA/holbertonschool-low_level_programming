8c2db78d4ef0441ee1e99518a30ce260b477f097
MariamCHARRADA <charradamariam@yahoo.com>
2023-06-22 01:54:20 -0700

    x10

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -9,9 +9,9 @@ void print_alphabet_x10(void)
 
 	for (n = 0; n < 10; n++)
 	{
-	 for (; ch <= 'z'; ch++)
-	 {	_putchar(ch);
-	 }
+		for (; ch <= 'z'; ch++)
+		{	_putchar(ch);
+		}
 	_putchar('\n');
 	}
 }
