3b1fdecdfbdef650ee18df3fde0b81429809e02b
MariamCHARRADA <charradamariam@yahoo.com>
2023-06-27 03:22:05 -0700

    put half

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -7,22 +7,22 @@
 void puts_half(char *str)
 {
 	int i, h;
-	h = strlen(str) - 1;
 
-		if (h % 2 == 0)
+	h = strlen(str) - 1;
+	if (h % 2 == 0)
+	{
+		for (i = h / 2; i <= h; i++)
 		{
-			 for (i = (h / 2); i <= h; i++)
-			 {
-				 _putchar(str[i]);
-			 }
-			 _putchar('\n');
+			 _putchar(str[i]);
 		}
-		else
+		_putchar('\n');
+	}
+	else
+	{
+		for (i = (h + 1) / 2; i <= h; i++)
 		{
-			for (i = (h + 1 / 2); i <= h; i++)
-			{
-				_putchar(str[i]);
-			}
-			_putchar('\n');
+			_putchar(str[i]);
 		}
+		_putchar('\n');
+	}
 }
