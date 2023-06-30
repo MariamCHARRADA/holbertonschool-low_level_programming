abc46c894b14b12167222ff33cf5c3e7ed41fbd2
MariamCHARRADA <charradamariam@yahoo.com>
2023-06-30 04:54:49 -0700

    lll

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * _strcpy - copy string
+ * @dest: destination
+ * @src: source
+ * Return: dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+	*dest = *src;
+	return (dest);
+}
