df6364715d90132147b3181451903c25f401d579
MariamCHARRADA <charradamariam@yahoo.com>
2023-06-30 04:23:33 -0700

    use a as the array

--- pointers_arrays_strings/8-print_array.c
+++ pointers_arrays_strings/8-print_array.c
@@ -9,10 +9,16 @@ void print_array(int *a, int n)
 {
 	int i = 0;
 
-	while (i < n)
+	for (; i < n; i++)
 	{
-		printf("%d, ", a[i]);
-		i++;
+		if (i != n -1)
+		{
+			printf("%d, ", a[i]);
+		}
+		else
+		{
+			printf("%d", a[i]);
+		}
 	}
 	printf("\n");
 }
