2b269ce2152fbac072bec85476d6530d1087a2fe
MariamCHARRADA <charradamariam@yahoo.com>
2023-07-02 12:13:36 -0700

    i is unsigned

---
+++ pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _memcpy - copies memory area
+ * @dest: destination
+ * @src: source
+ * @n: int
+ * Return: ptr to dest
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i = 0;
+
+	while (i < n)
+	{
+		dest[i] = src[i];
+		i++;
+		n--;
+	}
+	return (dest);
+}
