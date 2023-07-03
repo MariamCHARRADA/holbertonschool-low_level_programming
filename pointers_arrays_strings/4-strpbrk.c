63d47e8ab14059eaa2306c07cdab591b5d2ba3ec
MariamCHARRADA <charradamariam@yahoo.com>
2023-07-03 01:47:54 -0700

    fail

---
+++ pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * _strpbrk - searches a string for any of a set of bytes
+ * @s: string
+ * @accept: string
+ * Return: ptr to s or NULL
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	int j, i = 0;
+
+	while (s[i] != '\0')
+	{
+		j = 0;
+		while (accept[j] != '\0')
+		{
+		if (s[i] == accept[j])
+			break;
+		j++;
+		}
+	}
+	return (&s[i]);
+}
