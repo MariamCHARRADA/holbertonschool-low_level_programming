411ce5219a4183d7a2b89981349ca85d6fcd46d3
MariamCHARRADA <charradamariam@yahoo.com>
2023-07-03 04:44:40 -0700

    print matrix

--- pointers_arrays_strings/7-print_chessboard.c
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -1,20 +1,22 @@
 #include "main.h"
+#include <stdio.h>
 /**
  * print_chessboard - prints the chessboard
  * @a: array
- * @board: char input
  */
-void print_chessboard(char (*a)[8], char board)
+void print_chessboard(char (*a)[8])
 {
-	int i = 0, j = 0;
+	int i = 0, j;
 
 	while (i < 8)
 	{
+		j = 0;
 		while (j < 8)
 		{
-			a[i][j] = board;
+			putchar(a[i][j]);
 			j++;
 		}
+	putchar('\n');
 	i++;
 	}
 }
