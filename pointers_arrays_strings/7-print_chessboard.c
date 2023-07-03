63f3d4f22a3532defb6e429dad5e670205436c27
MariamCHARRADA <charradamariam@yahoo.com>
2023-07-03 04:11:56 -0700

    add char c in prototype

---
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_chessboard - prints the chessboard
+ * @a: array
+ */
+void print_chessboard(char (*a)[8], char c)
+{
+	int i = 0, j = 0;
+
+	while (i < 8)
+	{
+		while (j < 8)
+		{
+			a[i][j] = c;
+			j++;
+		}
+	i++;
+	}
+}
