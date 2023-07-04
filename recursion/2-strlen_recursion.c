3efe5327ded83b020391ca12a47c53ccdc467a9f
MariamCHARRADA <charradamariam@yahoo.com>
2023-07-04 04:34:05 -0700

    return (_strlen_recursion(s + 1) + 1) sinon return 0

--- recursion/2-strlen_recursion.c
+++ recursion/2-strlen_recursion.c
@@ -6,15 +6,9 @@
  */
 int _strlen_recursion(char *s)
 {
-	int len = 0;
-
 	if (*s != '\0')
 	{
-		len++;
-		_strlen_recursion(s + 1);
-		return (len);
+		return (_strlen_recursion(s + 1) + 1);
 	}
-	/* return (len); */
-	else
-		return (0);
+	return (0);
 }
