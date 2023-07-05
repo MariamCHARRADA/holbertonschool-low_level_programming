ab68d9350d762ca758872087647e12494d787404
MariamCHARRADA <charradamariam@yahoo.com>
2023-07-04 21:12:12 -0700

    #include <math.h>

---
+++ recursion/6-is_prime_number.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <math.h>
+/**
+ * is_prime_number - returns 1 if input int is a prime number, else 0
+ * @n: number
+ * Return: 1 or 0
+ */
+int is_prime_number(int n)
+{
+	int i = 2;
+
+	if (i * i <= n)
+	{
+		if (i * i == n && i != sqrt(n))
+			return (1);
+	is_prime_number(i + 1);
+	}
+	return (0);
+}
