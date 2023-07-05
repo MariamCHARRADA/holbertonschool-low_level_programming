725b9c241150dcc06c27a5b9030cf8f05a187699
MariamCHARRADA <charradamariam@yahoo.com>
2023-07-04 20:46:09 -0700

    prfc root if i * i == n

---
+++ recursion/5-sqrt_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _sqrt_recursion - returns the natural square root of a number
+ * @n: number
+ * Return: natural square root of n
+ */
+int _sqrt_recursion(int n)
+{
+	int i = 1;
+
+	if (i * i <= n)
+	{
+		if (i * i == n)
+			return (i);
+	_sqrt_recursion(i + 1);
+	}
+	return (-1);
+}
