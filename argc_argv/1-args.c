790b5e63833f08be164cd8aefab05594238339a4
MariamCHARRADA <charradamariam@yahoo.com>
2023-07-06 06:44:26 -0700

    (void)argv;

---
+++ argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include"main.h"
+/**
+ * main - prints the number of arguments passed into it
+ * @argv: argv
+ * @argc: argc
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	(void)argv;
+
+	printf("%d\n", argc);
+	return (0);
+}
