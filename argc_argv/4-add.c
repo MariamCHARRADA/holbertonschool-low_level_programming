309c1478ecd670d285fbd556ef2f059a17794163
MariamCHARRADA <charradamariam@yahoo.com>
2023-07-06 13:04:47 -0700

    ugh

--- argc_argv/4-add.c
+++ argc_argv/4-add.c
@@ -10,7 +10,7 @@ int main(int argc, char *argv[])
 {
 	int i = 1;
 
-	while ((i < argc) && (atoi(argv[i]) >= '0') && (atoi(argv[i]) <= '9'))
+	while ((i < argc) && (atoi(argv[i]) >= 0) && (atoi(argv[i]) <= 9))
 	{
 		if (atoi(argv[i]) >= 0)
 		{
