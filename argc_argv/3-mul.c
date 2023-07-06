f19a3723f18cd0bd6c2c6458de5b724eb0e2732d
MariamCHARRADA <charradamariam@yahoo.com>
2023-07-06 07:56:26 -0700

    multiply

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -1,4 +1,5 @@
 #include"main.h"
+#include<stdlib.h>
 /**
  * main - multiply 2 numbers
  * @argv: argv
@@ -9,7 +10,7 @@ int main(int argc, char *argv[])
 {
 	if (argc == 3)
 	{
-		printf("%d\n", *argv[1] * (*argv[2]));
+		printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
 		return (0);
 	}
 	else
