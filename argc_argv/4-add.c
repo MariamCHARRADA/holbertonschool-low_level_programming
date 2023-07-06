d762c58d57c3744b9e96b502ecc0d04730898a07
MariamCHARRADA <charradamariam@yahoo.com>
2023-07-06 12:55:20 -0700

    ugh

---
+++ argc_argv/4-add.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include<stdlib.h>
+/**
+ * main - adds numbers
+ * @argc: arg count
+ * @argv: arg vector
+ * Return: 0 or 1
+ */
+int main(int argc, char *argv[])
+{
+	int i = 1;
+
+	while ((i < argc) && (atoi(argv[i]) >= '0') && (atoi(argv[i]) <= '9'))
+	{
+		if (atoi(argv[i]) >= 0)
+		{
+			printf("%d\n", atoi(argv[i]) + atoi(argv[i + 1]));
+			return (0);
+		}
+		else
+		{
+			printf("%d\n", 0);
+		}
+		i++;
+	}
+	printf("%s", "Error");
+return (1);
+}
