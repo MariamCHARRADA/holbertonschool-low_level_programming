46336ed2bc0006e6c47ae61fa570d9e8067b9f88
MariamCHARRADA <charradamariam@yahoo.com>
2023-07-10 14:53:11 -0700

    ff

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -9,13 +9,20 @@
 char *str_concat(char *s1, char *s2)
 {
 	char *s;
+	int i = strlen(s1);
 
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
-	s = malloc((strlen(s1) + strlen(s2) + 2) * sizeof(char));
-	if (s1 && s2)
-		s += *s1 + *s2;
+	s = malloc((strlen(s1) + strlen(s2) + 1) * sizeof(char));
+		strcpy(s, s1);
+		i = strlen(s1);
+		while (s2[i] != '\0')
+		{
+			s[i] = s2[i];
+			i++;
+		}
+	s[i] = '\0';
 	return (s);
 }
