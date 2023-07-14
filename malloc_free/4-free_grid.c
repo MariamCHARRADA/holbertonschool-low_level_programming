efd96aa50f8598f30ca57d74d44cb2aa53b637e0
MariamCHARRADA <charradamariam@yahoo.com>
2023-07-14 02:22:03 -0700

    free(grid[i])

--- malloc_free/4-free_grid.c
+++ malloc_free/4-free_grid.c
@@ -6,14 +6,11 @@
  */
 void free_grid(int **grid, int height)
 {
-	int i, j;
+	int i;
 
 	for (i = 0; i < height; i++)
 	{
-		for (j = 0; i < j; j++)
-		{
-			free(grid[j]);
-		}
+		free(grid[i]);
 	}
 	free(grid);
 }
