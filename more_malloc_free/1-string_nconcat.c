ec52be33f3c3f70e53980bed2442edeb2ed3f16f
MariamCHARRADA <charradamariam@yahoo.com>
2023-07-16 15:29:25 -0700

    null terminator in the end

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -16,7 +16,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
-	p = malloc((strlen(s1) + n) * sizeof(char));
+	p = malloc((strlen(s1) + n + 1) * sizeof(char));
 	if (p == NULL)
 		return (NULL);
 	strcpy(p, s1);
