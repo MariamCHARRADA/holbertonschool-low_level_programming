bb9165b8bc475e702ff020d6862fb6218a2c1d5b
MariamCHARRADA <charradamariam@yahoo.com>
2023-07-17 03:04:36 -0700

    header

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,6 +1,14 @@
-#ifndef
-#define
-struct dog {
+#ifndef _dog_
+#define _dog_
+/**
+  * struct dog - dog info
+  * @name: dog name
+  * @age: dog age
+  * @owner: dog owner
+  * struct dog: gets the information of each dog
+ */
+struct dog
+{
 	char *name;
 	float age;
 	char *owner;
