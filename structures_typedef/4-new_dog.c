5454885bd745cc78c5a1e16e32dbe732fcb273fc
MariamCHARRADA <charradamariam@yahoo.com>
2023-07-18 03:31:17 -0700

    malloc

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -7,13 +7,19 @@
  * @name: dog name
  * @age: dog age
  * @owner: dog owner
+ * Return: pointer or NULL
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *d;
 
-	d=malloc(strlen(name) * sizeof(char) +sizeof(age) * sizeof(float) + strlen(owner) * sizeof(char));
+	d = malloc(strlen(name) * sizeof(char)
+	+ sizeof(age) * sizeof(float) + strlen(owner)
+	* sizeof(char));
 	if (d == NULL)
 		return (NULL);
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
 	return (d);
 }
