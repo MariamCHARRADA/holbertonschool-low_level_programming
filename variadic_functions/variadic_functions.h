21343835b4f44993dda2627f77469a611ee094c5
MariamCHARRADA <charradamariam@yahoo.com>
2023-07-19 14:36:39 -0700

    missing

---
+++ variadic_functions/variadic_functions.h
@@ -0,0 +1,7 @@
+#ifndef _variadic_
+#define _variadic_
+int _putchar(char c);
+int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
+#endif
