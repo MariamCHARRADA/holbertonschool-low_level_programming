00cbd34c2afa28c29ec1e675224ebe12cb179d48
MariamCHARRADA <charradamariam@yahoo.com>
2023-07-19 15:54:56 -0700

    printf allowed

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -11,11 +11,11 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	unsigned int i;
 	va_list arg;
 
-if (separator != NULL)
-{
+	if (separator == NULL)
+		separator = "";
 	va_start(arg, n);
 	for (i = 0; i < n - 1; i++)
 		printf("%d%s", va_arg(arg, int), separator);
 	printf("%d\n", va_arg(arg, int));
-}
+
 }
