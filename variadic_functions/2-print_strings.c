855521b7dabdda4c4a9216c8762813be7d468a4f
MariamCHARRADA <charradamariam@yahoo.com>
2023-07-20 02:23:39 -0700

    char *

--- variadic_functions/2-print_strings.c
+++ variadic_functions/2-print_strings.c
@@ -12,7 +12,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	unsigned int i;
 
 	va_start(arg, n);
-	for (i = 0; i < n; i++)
+	for (i = 0; i < n - 1; i++)
 	{
 		printf("%s%s ", va_arg(arg, char *), separator);
 	}
