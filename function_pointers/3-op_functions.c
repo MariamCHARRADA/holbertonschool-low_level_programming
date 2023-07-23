d929a4ea8f62152c3655d62648354322fa6232a0
MariamCHARRADA <charradamariam@yahoo.com>
2023-07-23 01:51:50 -0700

    test

---
+++ function_pointers/3-op_functions.c
@@ -0,0 +1,58 @@
+#include "3-calc.h"
+#include <stdrlib.h>
+/**
+ * op_add - adds two integers
+ * @a: int
+ * @b: int
+ * Return: sum or NULL
+ */
+int op_add(int a, int b)
+{
+	if (a != NULL && b != NULL)
+		return (a + b);
+	return (NULL);
+}
+/**
+ * op_sub - subtitute
+ * @a: int
+ * @b: int
+ * Return: difference or NULL
+ */
+int op_sub(int a, int b)
+{
+	if (a != NULL && b != NULL)
+		return (a + b);
+	return (NULL);
+}
+/**
+ * op_add - multiplies two integers
+ * @a: int
+ * @b: int
+ * Return: product or NULL
+ */
+int op_mul(int a, int b)
+{
+	if (a != NULL)
+		return (a * b);
+	return (NULL);
+}
+/**
+ * op_div - devision
+ * @a: int
+ * @b: int
+ * Return: devision result or NULL
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * op_mod - devision remainder
+ * @a: int
+ * @b: int
+ * Return: remainder or NULL
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
