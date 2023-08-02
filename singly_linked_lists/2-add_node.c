6d75cd2eb077cfb83fb8470152ea0172e97a5a00
MariamCHARRADA <charradamariam@yahoo.com>
2023-08-02 04:41:42 -0700

    test

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -10,7 +10,7 @@ list_t *add_node(list_t **head, const char *str)
 	list_t *new_node;
 
 	new_node = malloc(sizeof(list_t));
-	if (str == NULL)
+	if (str == NULL || new_node == NULL)
 		return (NULL);
 	new_node->str = strdup(str);
 	if (new_node->str == NULL)
