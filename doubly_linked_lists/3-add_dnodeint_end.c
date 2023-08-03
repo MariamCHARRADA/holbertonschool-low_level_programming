d79c32547820105bf2aace8db5c2ce558b2c6e37
MariamCHARRADA <charradamariam@yahoo.com>
2023-08-03 06:26:27 -0700

    null head check

--- doubly_linked_lists/3-add_dnodeint_end.c
+++ doubly_linked_lists/3-add_dnodeint_end.c
@@ -18,7 +18,7 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 	if (*head == NULL)
 	{
 		*head = end;
-		end->prev = head;
+		end->prev = *head;
 		(*head)->prev = NULL;
 	}
 	current = *head;
