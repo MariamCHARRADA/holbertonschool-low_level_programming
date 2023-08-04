893405a2176f260c1b8082afd6ecf492d621ea1a
MariamCHARRADA <charradamariam@yahoo.com>
2023-08-04 03:59:08 -0700

    kernel fix

--- doubly_linked_lists/6-sum_dlistint.c
+++ doubly_linked_lists/6-sum_dlistint.c
@@ -1,6 +1,6 @@
 #include "lists.h"
 /**
- * sum_dlistint - returns the sum of all the data (n) of a dlistint_t linked list
+ * sum_dlistint - returns the sum of data of a dlistint_t linked list
  * @head: pointer to the head of the list
  * Return: sum or 0
  */
@@ -8,7 +8,7 @@ int sum_dlistint(dlistint_t *head)
 {
 	int sum = 0;
 	dlistint_t *current;
-	
+
 	current = head;
 	while (current)
 	{
