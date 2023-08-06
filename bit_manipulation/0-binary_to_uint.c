2f22051bc2d8d85d85be98160d67f628e2c924dd
MariamCHARRADA <charradamariam@yahoo.com>
2023-08-06 14:34:34 -0700

    null elim

--- bit_manipulation/0-binary_to_uint.c
+++ bit_manipulation/0-binary_to_uint.c
@@ -9,8 +9,6 @@ unsigned int binary_to_uint(const char *b)
 	unsigned int i = 0;
 	unsigned int decimal = 0;
 
-	if (b == NULL)
-		return (0);
 	while (b[i] == '1' || b[i] == '0')
 	{
 		decimal += b[i] * 2 ^ i;
