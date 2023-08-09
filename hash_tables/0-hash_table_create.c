3e745cf64d6983dcd1a899dcc9c590e0f85295c8
MariamCHARRADA <charradamariam@yahoo.com>
2023-08-09 16:36:49 -0700

    array malloc

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -2,18 +2,24 @@
 /**
  * hash_table_create - creates a hashtable
  * @size: the size of the array
- * Return: a pointer to the newly created hash table
+ * Return: a pointer to the newly created hash table or NULL
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
+	unsigned long int i = 0;
 	hash_table_t *table;
 
 	table = malloc(sizeof(hash_table_t));
-	
+
 	if (table == NULL)
 		return (NULL);
 
 	table->size = size;
+
+	table->array = malloc(size * sizeof(hash_table_t));
+
+	while (i < size)
+		table->array[i] = NULL;
 
 	return (table);
 }
