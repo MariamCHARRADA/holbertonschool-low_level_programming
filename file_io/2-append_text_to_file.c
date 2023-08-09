aab5eb83a5cf3183d4a2e7b5a47139725d4a79b8
MariamCHARRADA <charradamariam@yahoo.com>
2023-08-09 04:12:12 -0700

    wronly

--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -3,7 +3,7 @@
  * append_text_to_file - appends text at the end of a file
  * @filename: the file name
  * @text_content: NULL terminated string to append
- * Return: 1 on success, -1 on failure 
+ * Return: 1 on success, -1 on failure
 */
 int append_text_to_file(const char *filename, char *text_content)
 {
@@ -12,7 +12,7 @@ int append_text_to_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	o = open(filename, O_APPEND);
+	o = open(filename, O_APPEND | O_WRONLY);
 	if (o == -1)
 		return (-1);
 	if (text_content != NULL)
@@ -20,7 +20,7 @@ int append_text_to_file(const char *filename, char *text_content)
 		len = strlen(text_content);
 		w = write(o, text_content, len);
 	}
-	
+
 	if (w == -1)
 		return (-1);
 
