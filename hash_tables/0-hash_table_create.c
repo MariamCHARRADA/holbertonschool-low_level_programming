7ec42a379117e1cfe830d5423385ebccaa52402c
MariamCHARRADA <charradamariam@yahoo.com>
2023-08-09 16:24:37 -0700

    malloc return

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -9,6 +9,10 @@ hash_table_t *hash_table_create(unsigned long int size)
 	hash_table_t *table;
 
 	table = malloc(sizeof(hash_table_t));
+	
+	if (table == NULL)
+		return (NULL);
+
 	table->size = size;
 
 	return (table);
