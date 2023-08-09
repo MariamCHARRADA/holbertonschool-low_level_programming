c52e70905d20e560b3a3f4a4aef7db415201bfbd
MariamCHARRADA <charradamariam@yahoo.com>
2023-08-09 02:06:40 -0700

    text fix

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -18,6 +18,9 @@ int create_file(const char *filename, char *text_content)
 
 	o = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 
+	if (o == -1)
+		return (-1);
+
 	if (text_content != NULL)
 		w = write(o, text_content, len);
 
