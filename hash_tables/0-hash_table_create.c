a001a3b483c26f959bf288a83bbd21753c74e67e
MariamCHARRADA <charradamariam@yahoo.com>
2023-08-09 16:43:49 -0700

    array malloc: array of pointers

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -14,12 +14,13 @@ hash_table_t *hash_table_create(unsigned long int size)
 	if (table == NULL)
 		return (NULL);
 
-	table->size = size;
-
-	table->array = malloc(size * sizeof(hash_table_t));
+	table->array = malloc(size * sizeof(hash_table_t *));
+	if (table->array == NULL)
+		return (NULL);
 
 	while (i < size)
 		table->array[i] = NULL;
 
+	table->size = size;
 	return (table);
 }
