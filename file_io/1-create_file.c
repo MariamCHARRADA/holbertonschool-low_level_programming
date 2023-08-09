b951853357d59f3eb7c1837ebb4f9f1c11422ddf
MariamCHARRADA <charradamariam@yahoo.com>
2023-08-09 02:12:02 -0700

    test

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -16,17 +16,17 @@ int create_file(const char *filename, char *text_content)
 
 	len = strlen(text_content);
 
-	o = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	o = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
 
 	if (o == -1)
 		return (-1);
 
 	if (text_content != NULL)
+	{
 		w = write(o, text_content, len);
-
-	if (o == -1 || w == -1)
-
-		return (-1);
+		if (w == -1)
+			return (-1);
+	}
 
 	close(o);
 	return (1);
