096b25607e559b5cbdb63ce040d4208d6a899d03
MariamCHARRADA <charradamariam@yahoo.com>
2023-08-11 04:00:18 -0700

    test

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -3,21 +3,29 @@
 /**
  * hash_table_set - adds an element to the hash table
  * @ht: the hash table to add or update the key/value to
- * @key: the ley string
+ * @key: the key string
  * @value: the value associated with the key
  * Return: 1 if it succeeded, 0 otherwise
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	hash_node_t *new;
+	hash_node_t *element;
+	unsigned long int index;
 
-	if (ht == NULL || *key == '\0' || key == NULL)
+	if (ht == NULL || key == NULL || *key == '\0')
 		return (0);
+	
 	new = malloc(sizeof(hash_node_t));
 	if (new == NULL)
 		return (0);
+	
+	index = key_index((const unsigned char *)key, ht->size);
+	element = ht->array[index];
+
 	new->key = strdup(key);
 	new->value = strdup(value);
-	new->next = NULL;
+	new->next = element;
+	ht->array[index] = new;
 	return (1);
 }
