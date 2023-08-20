67a57e93cc5e0418c803bdd6d70a117dc98ca0d6
MariamCHARRADA <charradamariam@yahoo.com>
2023-08-20 12:13:09 -0700

    char *sep

--- hash_tables/5-hash_table_print.c
+++ hash_tables/5-hash_table_print.c
@@ -5,16 +5,19 @@
  */
 void hash_table_print(const hash_table_t *ht)
 {
-	long unsigned int i = 0;
+	unsigned long int i = 0;
+	char *sep = "";
 
 	if (ht != NULL)
 	{
 		putchar('{');
+		sep = "";
 		while (i < ht->size)
 		{
 			while (ht->array[i])
 			{
-				printf("'%s': '%s', ", ht->array[i]->key, ht->array[i]->value);
+				printf("%s'%s': '%s'", sep, ht->array[i]->key, ht->array[i]->value);
+				sep = ", ";
 				i++;
 			}
 			i++;
