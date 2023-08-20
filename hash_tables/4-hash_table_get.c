6b6008e6ac842d5e941df142b0a2243501c6126d
MariamCHARRADA <charradamariam@yahoo.com>
2023-08-20 11:48:13 -0700

    return handled

--- hash_tables/4-hash_table_get.c
+++ hash_tables/4-hash_table_get.c
@@ -4,7 +4,7 @@
  * hash_table_get - retrieves a value associated with a key
  * @ht: the hash table to be looked into
  * @key: the key to be looked for
- * Return: the value associated with the element, or NULL if key couldn’t be found
+ * Return: the value associated with the element, or NULL
  */
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
