f510959243d07cb78c18745b6a1bad01f23a5636
MariamCHARRADA <charradamariam@yahoo.com>
2023-09-22 03:17:56 -0700

    fix format specifier fix

--- search_algorithms/0-linear.c
+++ search_algorithms/0-linear.c
@@ -16,10 +16,11 @@ int linear_search(int *array, size_t size, int value)
 
 	while (i < size)
 	{
-		printf("Value checked array[%d] = [%d]\n", i, array[i]);
+		printf("Value checked array[%li] = [%d]\n", i, array[i]);
 		if (array[i] == value)
 			return (i);
 		i++;
 	}
 
+	return (-1);
 }
